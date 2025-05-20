a70b4469e7234020c94bef6fadc2498633d5486a
Jeremy <200618875@student.georgianc.on.ca>
2025-05-20 11:35:57 -0400

    May 20th 11:35am Making Product Table

--- lesson-3/js/products.js
+++ lesson-3/js/products.js
@@ -19,19 +19,22 @@ let itemDetail = [];
 let itemDesc;
 let itemPrice;
 // STEP 2: Build a loop to iterate through the products array
-
-	// STEP 3: Break apart the product name from the price for each item with split()
-
-	// STEP 4: Now we have an array as an element of an array - set the second array element to the product price (as type number)
-
-	// STEP 5: Add the price of this product to the invoice total
-
-	// STEP 6: Capture each product name and price as variables 
-
-	// STEP 7: Create a TR element for this product and price in the invoice table
-
-	// STEP 8: Build the string that contains two TD elements each containing one of the item description, and the item price
-
-	// STEP 9: Set the above string as the innerHTML of the new TR element, and then append the new element to the table body (var productList)
-
+for(let i = 0; i < products.length; i++){
+    // STEP 3: Break apart the product name from the price for each item with split()
+    products[i] = products[i].split(": ");
+    // STEP 4: Now we have an array as an element of an array - set the second array element to the product price (as type number)
+    products[i][1] = Number(products[i][1]);
+    console.log(products[i][0] + " " + products[i][1]);
+    // STEP 5: Add the price of this product to the invoice total
+    invoiceTotal += products[i][1];
+    console.log(invoiceTotal);
+    // STEP 6: Capture each product name and price as variables
+    itemDesc = products[i][0];
+    itemPrice = products[i][1]
+    // STEP 7: Create a TR element for this product and price in the invoice table
+    itemRow[i] = document.createElement("tr");
+    // STEP 8: Build the string that contains two TD elements each containing one of the item description, and the item price
+    itemDetail[i] = `<td>${itemDesc}</td><td>$${itemPrice}</td>`;
+    // STEP 9: Set the above string as the innerHTML of the new TR element, and then append the new element to the table body (var productList)
+};
 // STEP 10: Set the total cost of the invoice as the textContent of the TD in the TFOOT (var totalData), rounding the number to two decimal places
